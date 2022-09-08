1a1be937ff6541e4ab4590d1e8c5f7c6ed19d0b5
Arbaj Sheikh <112559813+jd-arbaj@users.noreply.github.com>
2022-09-08 13:55:59 +0530

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,30 @@
+package com.testExam.bll;
+ 
+ import java.util.Scanner;
+public class CountDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int pin=2841;
+		int epin;
+		int count=0;
+		do {
+			System.out.println("Enter your Pin");
+			epin = sc.nextInt();
+			if(epin==pin) {
+				System.out.println("'Correct' welcome back");
+				break;
+			}
+			else {
+				System.out.println("Incorrect pin 'Try again'");
+				count++;
+			}
+		}while(count<3);
+		if(count==3) {
+			System.out.println("******************************");
+			System.out.println("Sorry you have been locked out");
+		}
+		sc.close();
+	}
+
+}
